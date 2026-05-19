81ce9a6218478a58693ce2163af04db101ce8437
unknown <joao.vgl@puccampinas.edu.br>
2026-05-19 15:48:36 -0300

    Criação da função de atualizar pedidos. Implementando atualizar status e remover pedidos.

--- main2.py
+++ main2.py
@@ -58,6 +58,91 @@ def cadastrarEntregador():
     print(listaEntregadores)
     print('Entregador cadastrado com sucesso')
     input('Pressione ENTER para continuar')
+
+def atualizarPedido():
+    print('''
+        1. Alterar o status do pedido
+        2. Cancelar Pedido
+        3. Associar entregadores a pedidos
+        4. Remover associação de entregador
+''')
+    
+    n = -1
+
+    while n != 0:
+
+        os.system('cls')
+        atualizarPedido()
+        n = int(input("-> "))
+
+        if (n == 1):
+
+            idPedido = input('ID do pedido a ser atualizado: ')
+            while idPedido not in [pedido['idPedido'] for pedido in listaPedidos]:
+                print('ID do pedido nao encontrado. Digite um ID valido.')
+                idPedido = input('ID do pedido a ser atualizado: ')
+            
+            print('''
+                1. Pendente
+                2. Em Rota
+                3. Entregue
+                4. Cancelado
+            ''')
+            status = int(input('Novo status do pedido: '))
+
+            if status == 1:
+                for pedido in listaPedidos:
+                    if pedido['idPedido'] == idPedido:
+                        pedido['status'] = 'Pendente'
+                        print('Status do pedido atualizado para Pendente')
+                        menu()
+            elif status == 2:
+                for pedido in listaPedidos:
+                    if pedido['idPedido'] == idPedido:
+                        pedido['status'] = 'Em Rota'
+                        print('Status do pedido atualizado para Em Rota')
+                        menu()
+            elif status == 3:
+                for pedido in listaPedidos:
+                    if pedido['idPedido'] == idPedido:
+                        pedido['status'] = 'Entregue'
+                        print('Status do pedido atualizado para Entregue')
+                        menu()
+            elif status == 4:
+                for pedido in listaPedidos:
+                    if pedido['idPedido'] == idPedido:
+                        pedido['status'] = 'Cancelado'
+                        print('Status do pedido atualizado para Cancelado')
+                        menu()
+            else:
+                print('Opcao invalida. Status não será atualizado.')
+            
+            input('Pressione ENTER para continuar')
+
+        if (n == 2):
+
+            idPedido = input('ID do pedido a ser cancelado: ')
+            while idPedido not in [pedido['idPedido'] for pedido in listaPedidos]:
+                print('ID do pedido nao encontrado. Digite um ID valido.')
+                idPedido = input('ID do pedido a ser cancelado: ')
+            
+            for pedido in listaPedidos:
+                if pedido['idPedido'] == idPedido:
+                    print(f'Pedido encontrado: {pedido}')
+                    n = str(input("Deseja mesmo cancelar esse pedido? (s/n) -> "))
+
+                    while n.lower() != 's' and n != 'n':
+                        print('Opcao invalida. Digite s para sim ou n para nao.')
+                        n = str(input("Deseja mesmo cancelar esse pedido? (s/n) -> "))
+                    if n.lower() == 's':
+                        listaPedidos.remove(pedido)
+                        input('Pedido cancelado com sucesso. Pressione ENTER para continuar')
+
+                    elif n.lower() == 'n':
+                        print('Seu pedido não será cancelado. Pressione ENTER para continuar.')
+                        menu()
+
+                    
  
 
 def menu():
@@ -90,4 +175,6 @@ def main():
             cadastrarPedido();
         elif opcao == 2:
             cadastrarEntregador();
+        elif opcao == 3:
+            atualizarPedido();
 main()
